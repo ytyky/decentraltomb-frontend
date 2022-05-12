0069731b8a8ff6e2e8758d7832d020017a4a64cb
Tianyu Yao <yaotianyu@berkeley.edu>
2022-05-12 03:15:13 -0400

    config contract address and abi

--- src/contract.js
+++ src/contract.js
@@ -4,10 +4,12 @@ const abi = require("./abi.json");
 
 const McpFunc = new Mcp();
 
-McpFunc.Contract.setProvider("http://18.182.45.18:8765");
+McpFunc.Contract.setProvider("http://13.212.177.203:8765");
 
-const tokenAddress = "0x56f12EC4980d7bbD0470BEb3E713e61a33a1141C";
-const coreAddress = "0x56f12EC4980d7bbD0470BEb3E713e61a33a1141C";
+
+// Tomb address
+const tokenAddress = "0x8211a9645750B8322728c9e67e7872042d9f36ba";
+const coreAddress = "0x8211a9645750B8322728c9e67e7872042d9f36ba";
 
 const Instance = new McpFunc.Contract(
     abi,
